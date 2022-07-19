bd91356331bded4ac1a609afb6f44a6ba0101654
Fabrowser <79614829+Fabrowser@users.noreply.github.com>
2022-07-19 00:00:39 -0300

    Add files via upload

---
+++ Produto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Vetor_Objeto
+{
+    class Produto
+    {
+
+        public string Name{ get; set; }
+        public double Price { get; set; }
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Vetor_Objeto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double preco_medio=0;
+            int n = int.Parse(Console.ReadLine());
+
+            Produto[] vect = new Produto[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Digite o nome do produto: ");
+                string nome = Console.ReadLine();
+
+                Console.WriteLine("Digite o preço do produto: ");
+                double preco = double.Parse(Console.ReadLine());
+                vect[i] = new Produto
+                {
+                    Name = nome,
+                    Price = preco
+                };
+
+                preco_medio += preco/n;
+
+            }
+
+            Console.WriteLine("Preço médio = " + preco_medio);
+
+
+        }
+    }
+}
